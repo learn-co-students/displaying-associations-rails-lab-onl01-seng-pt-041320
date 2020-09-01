6d159eba602591a3dac823b5f9687ae4d5931ed2
christinezosche <christine.zosche@gmail.com>
2020-08-31 18:18:02 -0700

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,11 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Artist.find(params[:id])
+    @artist_songs = Song.all.find_by(artist: @artist)
   end
 
   def new
